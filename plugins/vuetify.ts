fd1336045469fbe16b9413b8883ce5538e7812cb
some-user123 <74715856+some-user123@users.noreply.github.com>
2022-08-01 10:22:47 +0200

    Update vuetify.ts

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,8 +1,11 @@
 import { defineNuxtPlugin } from '#app'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
 
 export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify();
+  const vuetify = createVuetify({
+    components
+  })
 
-  app.vueApp.use(vuetify);
+  app.vueApp.use(vuetify)
 })
