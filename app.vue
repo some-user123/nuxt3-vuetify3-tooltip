aa732b237371ce98e8017800dedded204cc76409
some-user123 <74715856+some-user123@users.noreply.github.com>
2022-08-01 10:02:27 +0200

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-btn>
+        <v-tooltip
+          activator="parent"
+          location="bottom"
+        >
+          Tooltip
+        </v-tooltip>
+        Button
+      </v-btn>
+
+      <!-- 
+      <v-tooltip
+        location="bottom"
+      >
+        <template #activator="{ props }">
+          <v-btn v-bind="props">
+            Button
+          </v-btn>
+        </template>
+        <span>Tooltip</span>
+      </v-tooltip> -->
+    </v-container>
+  </v-app>
+</template>
